7bf009b0d79eab6b819894a3a5107663c1fb5f38
Jordan Cazares <l.caej040411@itses.edu.mx>
2025-10-07 18:10:36 -0600

    Navbar lateral

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'navbar',
+        component: NavbarComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'navbar'
+    }
+];
